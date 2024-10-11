4af1629000cea9d2f104701c864ce83c30c7e400
constance khalumba <constancekhalumba@gmail.com>
2024-10-11 09:22:58 +0300

    Fisrt commit

---
+++ djangodb/website/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import User
+
+def home(request):
+    all_users = User.objects.all
+    return render(request, 'home.html', {'all': all_users})
+
+def join(request):
+    return render(request, 'join.html', {})
